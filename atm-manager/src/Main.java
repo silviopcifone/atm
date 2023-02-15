11d9e48f0338914d0d8e9ec7259922aa1237231b
Silvio <106682007+silviopcifone@users.noreply.github.com>
2023-02-15 11:43:10 +0100

    Add Menu and user Input

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double balance = 10000;
-        double depositAmount = 0;
-        int  withdrawAmount = 0;
+        double balanceAmount = 10000;
+        int depositAmount = 0, withdrawAmount = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to ATM ... ");
+
+        System.out.println("Select 1 for Withdraw");
+        System.out.println("Select 2 for Deposit");
+        System.out.println("Select 3 for Check Balance");
+        System.out.println("Select 4 for EXIT");
+        System.out.print("Select the appropriate options you want to perform: ");
+
+        byte operation = scanner.nextByte();
+
     }
 }
