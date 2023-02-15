382657244ee6ed33e24f3e8ce854a65921ef5c62
Chiara <chiaratasnim@live.it>
2023-02-15 11:34:30 +0100

    Aggiunta del messaggio di benvenuto e menù

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         double saldo = 0;
         int prelievo = 0;
         double deposito = 0;
+        System.out.println("Benvenuto! Seleziona una delle voci dell'ATM");
+        System.out.println("1. Saldo disponibile");
+        System.out.println("2. Prelievo");
+        System.out.println("3. Deposito");
+        System.out.println("Inserire il numero dell'operazione da eseguire: ");
+        byte opzione = input.nextByte();
     }
 }
