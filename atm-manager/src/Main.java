1919014948054a0943ddf3eac01cd05e4d44b3f3
MattiaNoce <mattia.noce.5@gmail.com>
2023-02-15 12:42:24 +0100

    improve code

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -9,24 +9,36 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Benvenuto");
-        String menu = ("seleziona l'operazione che vuoi eseguire: \n1: BALANCE\n2: DEPOSIT\n3: WHITDRAW\n4:EXIT");
+        String menu = ("seleziona l'operazione che vuoi eseguire: " +
+                "\n1: BALANCE" +
+                "\n2: DEPOSIT" +
+                "\n3: WHITDRAW" +
+                "\n4:EXIT");
         System.out.println(menu);
 
         int operation = 0;
+
         while (operation != 4) {
-            operation= scanner.nextInt();
+
+            System.out.println(menu);
+            operation = scanner.nextInt();
+
             switch (operation) {
+
                 case 1:
                     System.out.println("il tuo attuale bilancio è di " + balanceAmount);
                     break;
+
                 case 2:
                     System.out.println("inserisci la cifra che vuoi versare");
                     depositAmount += scanner.nextInt();
                     balanceAmount += depositAmount;
+                    System.out.println("hai versato in totale oggi " + depositAmount);
 
                     System.out.println("ti ringraziamo per il tuo deposito");
                     System.out.println("il tuo attuale bilancio è di " + balanceAmount);
                     break;
+
                 case 3:
                     System.out.println("inserisci la cifra da prelevare");
                     whitdrawAmount += scanner.nextInt();
@@ -36,13 +48,15 @@ public class Main {
                     } else {
                         balanceAmount -= whitdrawAmount;
                     }
-
+                    System.out.println("oggi hai prelevato un totale di "+ whitdrawAmount);
                     System.out.println("il tuo attuale bilanco è di " + balanceAmount);
                     break;
+
                 case 4:
                     System.out.println("grazie e arrivederci");
                     System.exit(0);
                     break;
+
                 default:
                     System.out.println("ERRORE");
                     System.out.println("inserisi un numero tra quelli sopra citati");
