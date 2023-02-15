eb3454c3144b82993daba71db4a518ed51a1519d
MattiaNoce <mattia.noce.5@gmail.com>
2023-02-15 12:26:10 +0100

    add while loop & correction error

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -9,38 +9,45 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Benvenuto");
-        String menu=("seleziona l'operazione che vuoi eseguire: \n1: BALANCE\n2: DEPOSIT\n3: WHITDRAW\n4:EXIT");
+        String menu = ("seleziona l'operazione che vuoi eseguire: \n1: BALANCE\n2: DEPOSIT\n3: WHITDRAW\n4:EXIT");
         System.out.println(menu);
 
-        int operation = scanner.nextInt();
+        int operation = 0;
+        while (operation != 4) {
+            operation= scanner.nextInt();
+            switch (operation) {
+                case 1:
+                    System.out.println("il tuo attuale bilancio è di " + balanceAmount);
+                    break;
+                case 2:
+                    System.out.println("inserisci la cifra che vuoi versare");
+                    depositAmount += scanner.nextInt();
+                    balanceAmount += depositAmount;
 
-        switch (operation) {
-            case 1:
-                System.out.println(balanceAmount);
-                break;
-            case 2:
-                System.out.println("inserisci la cifra che vuoi versare");
-                balanceAmount += scanner.nextInt();
-                depositAmount += scanner.nextInt();
-                System.out.println("ti ringraziamo per il tuo deposito");
-                System.out.println("il tuo deposito è di "+balanceAmount);
-                break;
-            case 3:
-                System.out.println("inserisci la cifra da prelevare");
-                whitdrawAmount= scanner.nextInt();
-                if(whitdrawAmount>balanceAmount){
-                    System.out.println("ipossibbile prlevare una cifra superiore a quella posseduta");
-                }else {
-                    balanceAmount-=whitdrawAmount;
-                }
-                System.out.println(balanceAmount);
-            case 4:
-                System.out.println("grazie e arrivederci");
-                break;
-            default:
-                System.out.println("ERRORE");
-                System.out.println("inserisi un numero tra quelli sopra citati");
+                    System.out.println("ti ringraziamo per il tuo deposito");
+                    System.out.println("il tuo attuale bilancio è di " + balanceAmount);
+                    break;
+                case 3:
+                    System.out.println("inserisci la cifra da prelevare");
+                    whitdrawAmount += scanner.nextInt();
 
+                    if (whitdrawAmount > balanceAmount) {
+                        System.out.println("ipossibbile prlevare una cifra superiore a quella posseduta");
+                    } else {
+                        balanceAmount -= whitdrawAmount;
+                    }
+
+                    System.out.println("il tuo attuale bilanco è di " + balanceAmount);
+                    break;
+                case 4:
+                    System.out.println("grazie e arrivederci");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("ERRORE");
+                    System.out.println("inserisi un numero tra quelli sopra citati");
+
+            }
         }
     }
 }
