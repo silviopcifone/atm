e287630a1bf2e6aecf5d7ad4f56fca6c4056629c
Emanuele <emanuele_valentini@hotmail.it>
2023-02-15 11:34:14 +0100

    Update Main.java

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
@@ -5,5 +7,13 @@ public class Main
         double balanceAmount = 10000;
         double depositAmount = 0;
         int withdrawalAmount = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Benvenuto nell'atm");
+        System.out.println("cosa vuoi fare? \n 1) Saldo contabile \n 2) Deposito \n 3) Prelievo \n 4) Esci");
+        System.out.println("Premi il tasto corrispondente all'operazione che vuoi effettuare");
+        byte choice = sc.nextByte();
+
+        
     }
 }
