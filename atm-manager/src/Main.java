d3622747312125090205bf63557cb093c2c69ae5
Lorenzo <lorenzom04@gmail.com>
2023-02-15 10:37:34 +0100

    Add primitive variables
    Declaration and inizialization of balance, withdraw and deposit

---
+++ atm-manager/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       double balance = 10000;
+       int withdrawAmount = 0;
+       double depositAmount = 0;
+
+    }
+}
