8f5232b050eb4ecc0592020889a4c98d6be2f61e
Emanuele <emanuele_valentini@hotmail.it>
2023-02-15 12:29:22 +0100

    Cicle commit
    Added while loop to cicle the switch

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -9,42 +9,45 @@ public class Main
         int withdrawalAmount = 0;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Benvenuto nell'atm");
-        System.out.println("cosa vuoi fare? \n 1) Saldo contabile \n 2) Prelievo \n 3) Deposito \n 4) Esci");
-        System.out.println("Premi il tasto corrispondente all'operazione che vuoi effettuare");
-        byte choice = sc.nextByte();
-
-        switch (choice)
+        while(true)
         {
-            case 1:
-                System.out.printf("Il saldo corrente è : %s", balanceAmount);
-                break;
-            case 2:
-                System.out.println("quanto vuoi prelevare?");
-                withdrawalAmount = sc.nextInt();
-                if(withdrawalAmount>=balanceAmount)
-                {
-                    System.out.println("non puoi prelevare più del saldo");
+            System.out.println("Benvenuto nell'atm");
+            System.out.println("cosa vuoi fare? \n 1) Saldo contabile \n 2) Prelievo \n 3) Deposito \n 4) Esci");
+            System.out.println("Premi il tasto corrispondente all'operazione che vuoi effettuare");
+            byte choice = sc.nextByte();
+
+            switch (choice)
+            {
+                case 1:
+                    System.out.printf("Il saldo corrente è : %s", balanceAmount);
                     break;
-                }
-                else
-                {
-                    balanceAmount -= withdrawalAmount;
+                case 2:
+                    System.out.println("quanto vuoi prelevare?");
+                    withdrawalAmount = sc.nextInt();
+                    if(withdrawalAmount>=balanceAmount)
+                    {
+                        System.out.println("non puoi prelevare più del saldo");
+                        break;
+                    }
+                    else
+                    {
+                        balanceAmount -= withdrawalAmount;
+                        System.out.println("il tuo nuovo saldo è: "+balanceAmount);
+                    }
+                    break;
+                case 3:
+                    System.out.println("quanto vuoi depositare?");
+                    depositAmount = sc.nextDouble();
+                    balanceAmount += depositAmount;
                     System.out.println("il tuo nuovo saldo è: "+balanceAmount);
-                }
-                break;
-            case 3:
-                System.out.println("quanto vuoi depositare?");
-                depositAmount = sc.nextDouble();
-                balanceAmount += depositAmount;
-                System.out.println("il tuo nuovo saldo è: "+balanceAmount);
-                break;
-            case 4:
-                System.out.println("grazie per aver utilizzato l'atm");
-                break;
-            default:
-                System.out.println("Errore, hai selezionato una funzionalità inesistente");
-                break;
+                    break;
+                case 4:
+                    System.out.println("grazie per aver utilizzato l'atm");
+                    System.exit(0);
+                default:
+                    System.out.println("Errore, hai selezionato una funzionalità inesistente");
+                    break;
+            }
         }
     }
 }
