4fed823f9af745604d7afa965c66a8d368d6bbb5
MattiaNoce <mattia.noce.5@gmail.com>
2023-02-15 11:34:25 +0100

    adding menu

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         double balanceAmount = 10000;
         long depositAmount = 0;
         long whitdrawAmount = 0;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Benvenuto");
+        System.out.println("seleziona l'operazione che vuoi eseguire: \n1: BALANCE\n2: DEPOSIT\n3: WHITDRAW\n4:EXIT");
+
+        int operation = scanner.nextInt();
+
+        switch (operation)
+        case 1:
+            
     }
 }
