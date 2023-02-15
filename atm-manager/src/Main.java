f71855f1eb5c6e75f0cc27ec6824ea97fa586962
amaljom <jomonamal999@gmail.com>
2023-02-15 11:35:21 +0100

    first operations implemented

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         double depositAmount = 0;
         int withdrawAmount = 0;
 
+        System.out.println("Benvenuto in ATM");
+
+        boolean operationCompleted = false;
+        while(!operationCompleted){
+            System.out.println("Scegli l' operazione \n" + "1 --> Controlla bilancio\n" + "2 --> Prelievo\n" +
+                            "3 --> Deposita\n" + "4 --> Esci\n");
+            Scanner sc = new Scanner(System.in);
+            byte userChoice = sc.nextByte();
+            System.out.println("Hai scelto l' operazione: " + userChoice);
+        }
+
     }
 }
