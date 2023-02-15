74b35dee23ebec20644114692c962107f7c7e860
Chiara <chiaratasnim@live.it>
2023-02-15 11:50:32 +0100

    Abbellite un po' le scritte dello switch

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -11,28 +11,28 @@ public class Main {
         System.out.println("2. Prelievo");
         System.out.println("3. Deposito");
         System.out.println("4. Esci");
-        System.out.println("Inserire il numero dell'operazione da eseguire: ");
+        System.out.print("Inserire il numero dell'operazione da eseguire: ");
         byte opzione = input.nextByte();
 
         switch(opzione) {
             case 1:
-                System.out.println("Il tuo saldo disponibile è " + saldo);
+                System.out.println("Il tuo saldo disponibile è " + saldo + "€");
                 break;
             case 2:
-                System.out.println("Inserire l'importo da prelevare: ");
+                System.out.print("Inserire l'importo da prelevare: ");
                 byte prel = input.nextByte();
                 if (prel<saldo) {
-                    System.out.println("Hai prelevato " + prel);
+                    System.out.println("Hai prelevato " + prel + "€");
                     saldo -= prel;
-                    System.out.println("Il tuo saldo disponibile adesso è " + saldo );
+                    System.out.println("Il tuo saldo disponibile adesso è " + saldo + "€");
                 } else {
                     System.out.println("Non puoi prelevare un'importo superiore al saldo");
                 }
                 break;
             case 3:
-                System.out.println("Inserire l'importo da depositare");
+                System.out.print("Inserire l'importo da depositare");
                 byte dep = input.nextByte();
-                System.out.println("Hai depositato " + dep);
+                System.out.println("Hai depositato " + dep + "€");
                 saldo += dep;
                 System.out.println("Il tuo saldo disponibile adesso è " + saldo);
                 break;
@@ -40,7 +40,7 @@ public class Main {
                 System.out.println("Grazie per aver utilizzato questo ATM, arrivederci! ");
                 break;
             default:
-                System.out.println("Inserire un'opzione valida");
+                System.out.println("Errore, inserire di nuovo il numero dell'operazione da eseguire");
                 break;
         }
     }
