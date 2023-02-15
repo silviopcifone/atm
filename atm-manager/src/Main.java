1546b2f05980b492663a9e241085e45c8b9f1a5c
amaljom <jomonamal999@gmail.com>
2023-02-15 11:37:38 +0100

    implemented switch case with first operation

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -15,6 +15,23 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             byte userChoice = sc.nextByte();
             System.out.println("Hai scelto l' operazione: " + userChoice);
+
+            switch (userChoice){
+                case 1:
+                    System.out.println("Il bilancio residuo è: " + balance);
+                    break;
+                case 2:
+                    System.out.println("quanto vuoi prelevare?: ");
+                    break;
+                case 3:
+                    System.out.println("Il bilancio residuo è: 1");
+                    break;
+                case 4:
+                    System.out.println("Il bilancio residuo è: 2");
+                    break;
+                default:
+                    System.out.println("ritenta!");
+            }
         }
 
     }
