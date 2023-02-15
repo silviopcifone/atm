7e87924dfbdeca7f061b2dc24e46bdba5b0459fc
MattiaNoce <mattia.noce.5@gmail.com>
2023-02-15 10:38:05 +0100

    Creation Amount variable

---
+++ atm-manager/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double balanceAmount = 10000;
+        long depositAmount = 0;
+        long whitdrawAmount = 0;
+
+    }
+}
