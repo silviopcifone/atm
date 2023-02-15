29b5d24d3b5e15593b37120d0bb1ef2193b37e34
amaljom <jomonamal999@gmail.com>
2023-02-15 10:37:18 +0100

    Project created
    Project created and declared variables

---
+++ atm-manager/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double balance = 10000;
+        double depositAmount = 0;
+        int withdrawAmount = 0;
+
+    }
+}
