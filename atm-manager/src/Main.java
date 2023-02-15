98b782f2f4893c6880cc730e728b5190a2ddeeda
Chiara <chiaratasnim@live.it>
2023-02-15 12:40:32 +0100

    Aggiunto ciclo

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         double saldo = 1000;
         int prelievo = 0;
         double deposito = 0;
+
+        boolean ancora = true;
+        do {
         System.out.println("Benvenuto! Seleziona una delle voci dell'ATM");
         System.out.println("1. Saldo disponibile");
         System.out.println("2. Prelievo");
@@ -14,34 +17,40 @@ public class Main {
         System.out.print("Inserire il numero dell'operazione da eseguire: ");
         byte opzione = input.nextByte();
 
-        switch(opzione) {
-            case 1:
-                System.out.println("Il tuo saldo disponibile è " + saldo + "€");
-                break;
-            case 2:
-                System.out.print("Inserire l'importo da prelevare: ");
-                byte prel = input.nextByte();
-                if (prel<saldo) {
-                    System.out.println("Hai prelevato " + prel + "€");
-                    saldo -= prel;
-                    System.out.println("Il tuo saldo disponibile adesso è " + saldo + "€");
-                } else {
-                    System.out.println("Non puoi prelevare un'importo superiore al saldo");
-                }
-                break;
-            case 3:
-                System.out.print("Inserire l'importo da depositare");
-                byte dep = input.nextByte();
-                System.out.println("Hai depositato " + dep + "€");
-                saldo += dep;
-                System.out.println("Il tuo saldo disponibile adesso è " + saldo);
-                break;
-            case 4:
-                System.out.println("Grazie per aver utilizzato questo ATM, arrivederci! ");
-                break;
-            default:
-                System.out.println("Errore, inserire di nuovo il numero dell'operazione da eseguire");
-                break;
-        }
+            switch(opzione) {
+                case 1:
+                    System.out.println("Il tuo saldo disponibile è " + saldo + "€");
+                    ancora = false;
+                    break;
+                case 2:
+                    System.out.print("Inserire l'importo da prelevare: ");
+                    byte prel = input.nextByte();
+                    if (prel<=saldo) {
+                        System.out.println("Hai prelevato " + prel + "€");
+                        saldo -= prel;
+                        System.out.println("Il tuo saldo disponibile adesso è " + saldo + "€");
+                    } else {
+                        System.out.println("Non puoi prelevare un'importo superiore al saldo");
+                    }
+                    ancora = false;
+                    break;
+                case 3:
+                    System.out.print("Inserire l'importo da depositare");
+                    byte dep = input.nextByte();
+                    System.out.println("Hai depositato " + dep + "€");
+                    saldo += dep;
+                    System.out.println("Il tuo saldo disponibile adesso è " + saldo);
+                    ancora = false;
+                    break;
+                case 4:
+                    System.out.println("Grazie per aver utilizzato questo ATM, arrivederci! ");
+                    System.exit(0);
+                    ancora = false;
+                    break;
+                default:
+                    System.out.println("Errore");
+                    break;
+            }
+        } while(ancora);
     }
 }
