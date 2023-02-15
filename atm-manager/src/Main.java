ff734ca2ed4fbfec47486ec5106bfba7863e86a0
Emanuele <emanuele_valentini@hotmail.it>
2023-02-15 10:36:38 +0100

    Add Main class
    Only primitive variables declaration and initialization.

---
+++ atm-manager/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double balanceAmount = 10000;
+        double depositAmount = 0;
+        int withdrawalAmount = 0;
+    }
+}
