6309d77544a8eb7a5874d8d226b242c5f5dc6d84
Lorenzo <lorenzom04@gmail.com>
2023-02-15 11:35:10 +0100

    Second commit
    Print "welcome", menu, menu options

--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -5,6 +5,20 @@ public class Main {
        double balance = 10000;
        int withdrawAmount = 0;
        double depositAmount = 0;
+       int choice = 0;
+        System.out.println("Benvenuto!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Seleziona l' operazione che vuoi eseguire:\n" +
+                            "1 - Controlla bilancio\n" +
+                            "2 - Prelievo\n" +
+                            "3 - Deposito\n" +
+                            "4 - Esci\n");
 
+
+        choice = scanner.nextInt();
+        System.out.println("Operazione scelta:"+ choice);
+        
+
+        }
     }
 }
