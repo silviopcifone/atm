688715348052db6cc4287df2bc3bb5f61314c4e0
LuigiZannella <zannellaluigi@outlook.com>
2023-02-16 11:39:09 +0100

    Add Atm

---
+++ atm-manager/src/org/elis/atm/Atm.java
@@ -0,0 +1,31 @@
+package org.elis.atm;
+
+import java.util.Scanner;
+
+public class Atm {
+    Account userAccount;
+
+    public void printWelcome() {
+
+    }
+
+    public void printMenu() {
+
+    }
+
+    public void printBalance() {
+
+    }
+
+    public void withdraw (int amount) {
+
+    }
+
+    public void deposit (int amount) {
+
+    }
+
+    public void closeApp() {
+
+    }
+}
