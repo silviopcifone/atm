64382032befd3fa850d71c12030a96af903484ea
LuigiZannella <zannellaluigi@outlook.com>
2023-02-16 11:39:34 +0100

    Add Account

---
+++ atm-manager/src/org/elis/atm/Account.java
@@ -0,0 +1,20 @@
+package org.elis.atm;
+
+public class Account {
+
+    private final int ID;
+    private int balance;
+    private AccountOwner owner;
+
+    public Account() {
+        ID =1;
+    }
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance += balance;
+    }
+
+}
