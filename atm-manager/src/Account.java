b217cf7a6ff0e1d3314c54d11014a59b399c2675
Silvio <106682007+silviopcifone@users.noreply.github.com>
2023-02-16 11:38:01 +0100

    Add Account Management Class

---
+++ atm-manager/src/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+
+    private final int ID;
+    private int balance;
+    private AccountOwner owner;
+
+    public Account() {
+        ID = 1;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance += balance;
+    }
+}
