43a600ccf765833ef4095a818afafc6ca8342412
Silvio <106682007+silviopcifone@users.noreply.github.com>
2023-02-16 11:38:17 +0100

    Add Atm Management Class

---
+++ atm-manager/src/Atm.java
@@ -0,0 +1,30 @@
+public class Atm {
+    /**
+     *
+     */
+    private Account userAccount;
+
+    public void printWelcome() {
+
+    }
+
+    public void printMenu() {
+
+    }
+
+    public void printBalance() {
+
+    }
+
+    public void withdraw(int amount) {
+
+    }
+
+    public void deposit(int amount) {
+
+    }
+
+    public void closeApp() {
+
+    }
+}
