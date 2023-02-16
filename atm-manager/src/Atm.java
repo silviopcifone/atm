774f91bdf76d7af8af1703d824e4054337b915af
amaljom <jomonamal999@gmail.com>
2023-02-16 11:48:10 +0100

    class atm created

---
+++ atm-manager/src/Atm.java
@@ -0,0 +1,18 @@
+public class Atm {
+
+    Account userAccount;
+
+    public void menu(){
+
+    }
+    public void withdraw(int amount){
+
+    }
+    public void deposit(int amount){
+
+    }
+
+    public void exit(){
+
+    }
+}
--- atm-manager/src/Main.java
+++ atm-manager/src/Main.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         double balance = 10000;
         double depositAmount = 0;
         int withdrawAmount = 0;
-
+        
         System.out.println("Benvenuto in ATM");
 
         // Exit condition
         boolean operationCompleted = false;
         while(!operationCompleted){
-            System.out.println("Scegli l' operazione \n" + "1 --> Controlla bilancio\n" + "2 --> Prelievo\n" +
-                            "3 --> Deposita\n" + "4 --> Esci\n");
+            System.out.println("Scegli l' operazione \n"
+                            + "1 --> Controlla bilancio\n" 
+                            + "2 --> Prelievo\n"
+                            + "3 --> Deposita\n"
+                            + "4 --> Esci\n");
             Scanner sc = new Scanner(System.in);
             byte userChoice = sc.nextByte();
             System.out.println("Hai scelto l' operazione: " + userChoice);
@@ -47,4 +51,5 @@ public class Main {
         }
 
     }
+
 }
