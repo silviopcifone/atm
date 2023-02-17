c0a2f18e86634313e5611e2981143824893538b1
Raffaele <antoniopapaleo24@gmail.com>
2023-02-17 11:24:07 +0100

    Update TestAtm.java

--- atm-manager/src/test/TestAtm.java
+++ atm-manager/src/test/TestAtm.java
@@ -2,11 +2,36 @@ package test;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestAtm {
 
 
     @Test
-    public void testPrintBalance() {
+    public void testAddition() {
 
+        int x = 0;
+        int expected = 5;
+        int actual = addition(0,5);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSubtraction() {
+        int x = 0;
+        int expected = 5;
+        int actual = subtraction(0,5);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public int addition(int x, int addend) {
+    return x + addend;
+    }
+    @Test
+    public int subtraction(int x, int i) {
+        return x - i;
     }
 }
