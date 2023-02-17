e317d6196bf60a7bea46b778a15d63e96ed1ec42
Raffaele <antoniopapaleo24@gmail.com>
2023-02-17 11:36:01 +0100

    Update Test

---
+++ atm-manager/src/test/TestUser.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+import sdk.model.User;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestUser {
+
+    @Test
+    public void testUserCreation() {
+        String name = "Raffaele";
+        String surname = "Papaleo";
+        User user = new User(name, surname);
+        String expectedUsername = name + surname;
+        assertEquals(expectedUsername, user.getUsername());
+    }
+}
