558af4c04c0205f04539adf667fa4f7bd2f0f485
MattiaNoce <mattia.noce.5@gmail.com>
2023-02-17 11:22:09 +0100

    complete user

---
+++ atm-manager/src/org/atm/model/User.java
@@ -0,0 +1,47 @@
+package org.atm.model;
+
+public class User {
+    private final int ID;
+    private String name;
+    private String surname;
+    private String username;
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.username = name + surname;
+        this.ID = generate();
+    }
+
+    public int generate(){
+        return 1;
+    }
+    public int getID() {
+        return ID;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
