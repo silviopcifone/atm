64de3054bfb53d45767962cf6b80dfd856a0aabe
MattiaNoce <mattia.noce.5@gmail.com>
2023-02-17 11:50:16 +0100

    Creation Test in java

--- atm-manager/src/org/atm/model/Account.java
+++ atm-manager/src/org/atm/model/Account.java
@@ -2,9 +2,13 @@ package org.atm.model;
 
 public class Account {
 
-    private final int ID;
+    private  int ID;
     private int balance;
     private AccountOwner owner;
+
+    public Account(int initialBalance) {
+        this.balance = initialBalance;
+    }
 
     public Account() {
         ID = 1;
